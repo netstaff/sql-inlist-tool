79972152b971db3d8f7322c97f81de722d5707c5
Ryan McCormick <ryan@itjobsguy.com>
2015-09-17 14:41:48 -0500

    Add jQuery Plugin for Demo Site

---
+++ app/scripts/demo/jquery-demo.js
@@ -0,0 +1,18 @@
+/*!
+ * SQL In-List Demo Site
+ * Plugin for jQuery
+ * https://www.netstaffpro.com/
+ *
+ * Copyright 2015, 2015 Net Staff, LLC and other contributors
+ * Released under the MIT license
+ *
+ * Date: 2015-09-17
+ */
+'use strict';
+
+$(document).ready(function(){
+
+
+
+
+});
