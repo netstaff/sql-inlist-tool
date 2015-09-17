b474a594ce4e802114d74221d77a97af7ee4674a
Ryan McCormick <ryan@itjobsguy.com>
2015-09-17 12:52:16 -0500

    Lint check sql-inlist

--- app/scripts/sql-inlist/sql-inlist.js
+++ app/scripts/sql-inlist/sql-inlist.js
@@ -7,78 +7,36 @@
  *
  * Date: 2015-09-17
  */
-var MakeList = function(rawList) {
-  // Constructor Variables
-  this.rawList = rawList;
-  this.listArray = toArray(this.rawList.split('\n'), function(n) {
-    return n;
-  });
+'use strict';
 
-  // Return Item Count
-  MakeList.prototype.listCount = function() {
-    return this.listArray.length;
-  };
-
-  /* Format Controller
-   * ----------------------------
-   * Return formatted SQL In List:
-   * 1. 'numberfy': returns values in integer format
-   * 2. 'stringify': returns values in string format
-   * 3. 'listify': returns values as list */
-  MakeList.prototype.displayList = function(dType) {
-    switch (dType) {
-      case "1":
-        return numberfy(this.listArray);
-        break;
-      case "2":
-        return stringify(this.listArray);
-        break;
-      case "3":
-        return listify(this.listArray);
-        break;
-      default:
-        return numberfy(this.listArray);
-    }
-  };
-
-  MakeList.prototype.removeDuplicates = function() {
-    this.listArray = rmduplicates(this.listArray);
-    return true;
-  };
-
-  MakeList.prototype.removeFirst = function() {
-    this.listArray.splice(0, 1);
-    return true;
-  };
-
-  // Helper Functions
+ // Helper Functions
 
   function stringify(value) {
-    var tS = "",
+    var tS = '',
       i;
     for (i = 0; i < value.length; i++) {
-      tS += "'" + value[i] + "',";
+      tS += '\'' + value[i] + '\',';
     }
-    return "IN (" + tS.slice(0, -1) + ")";
-  };
+    return 'IN (' + tS.slice(0, -1) + ')';
+  }
 
   function numberfy(value) {
-    var tS = "",
+    var tS = '',
       i;
     for (i = 0; i < value.length; i++) {
-      tS += value[i] + ",";
+      tS += value[i] + ',';
     }
-    return "IN (" + tS.slice(0, -1) + ")";
-  };
+    return 'IN (' + tS.slice(0, -1) + ')';
+  }
 
   function listify(value) {
-    var tS = "",
+    var tS = '',
       i;
     for (i = 0; i < value.length; i++) {
-      tS += value[i] + "\n";
+      tS += value[i] + '\n';
     }
     return tS.slice(0, -1);
-  };
+  }
 
   /* Duplicate Removal Algorithm
    * Adapted from #569 "Performance" from
@@ -97,8 +55,7 @@ var MakeList = function(rawList) {
       }
     }
     return out;
-  };
-  
+  }
   /* Raw line-break seperated input conversion
    * to arrray based on method 'Grep' from
    * the jQuery project. Previous attempts from
@@ -119,5 +76,52 @@ var MakeList = function(rawList) {
       }
     }
     return matches;
+  }
+
+var MakeList = function(rawList) {
+  // Constructor Variables
+  this.rawList = rawList;
+  this.listArray = toArray(this.rawList.split('\n'), function(n) {
+    return n;
+  });
+
+
+  // Return Item Count
+  MakeList.prototype.listCount = function() {
+    return this.listArray.length;
   };
-}
+
+  /* Format Controller
+   * ----------------------------
+   * Return formatted SQL In List:
+   * 1. 'numberfy': returns values in integer format
+   * 2. 'stringify': returns values in string format
+   * 3. 'listify': returns values as list */
+  MakeList.prototype.displayList = function(dType) {
+    var result = '';
+    switch (dType) {
+      case '1':
+        result = numberfy(this.listArray);
+        break;
+      case '2':
+        result = stringify(this.listArray);
+        break;
+      case '3':
+        result = listify(this.listArray);
+        break;
+      default:
+        result = numberfy(this.listArray);
+    }
+    return result;
+  };
+
+  MakeList.prototype.removeDuplicates = function() {
+    this.listArray = rmDuplicates(this.listArray);
+    return true;
+  };
+
+  MakeList.prototype.removeFirst = function() {
+    this.listArray.splice(0, 1);
+    return true;
+  };
+};
